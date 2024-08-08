7e46c25a0d859491b8ad3f95d4176f7647c341f0
NaynaBisht1 <145500769+NaynaBisht@users.noreply.github.com>
2024-08-09 00:36:39 +0530

    RemoveDuplicates

--- src/StriverDSA/Arrays/RemoveDuplicates.java
+++ src/StriverDSA/Arrays/RemoveDuplicates.java
@@ -2,11 +2,18 @@ package StriverDSA.Arrays;
 
 public class RemoveDuplicates {
     public static void main(String[] args) {
-
+        int[] arr = {1,1,1,2,2,3,4,4,5,5,5};
+        int n = arr.length;
+        System.out.println(removeDuplicates(arr,n));
     }
     public static int removeDuplicates(int[] arr,int n) {
-
-
-
+        int i = 0;
+        for (int j = 1; j <n; j++) {
+            if(arr[j] != arr[i]){
+                arr[i+1] = arr[j];
+                i++;
+            }
+        }
+        return i+1;
     }
 }
