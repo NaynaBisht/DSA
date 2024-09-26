60406c059fe10b0bdff81b68c8c5bd3ab1ad9640
NaynaBisht1 <145502271+NaynaBisht@users.noreply.github.com>
2024-09-26 23:35:01 +0530

    Reversing a signed 32-bit intege

---
+++ src/LeetCode/ReverseInteger.java
@@ -0,0 +1,32 @@
+package LeetCode;
+
+import StriverDSA.BasicMaths.ReverseNumber;
+
+/*Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0*/
+public class ReverseInteger {
+    public static void main(String[] args) {
+        ReverseInteger obj = new ReverseInteger();
+        int num = -123;
+        System.out.println(obj.reverse(num));
+
+    }
+    public int reverse(int x) {
+        int revnum = 0;
+        int lastdig;
+
+        while (x!=0)
+        {
+            lastdig = x%10;
+
+            // Check for overflow: If revNum is outside the range where it could lead to overflow
+            if(revnum>Integer.MAX_VALUE/10 || revnum>Integer.MAX_VALUE/10 && lastdig > 7)
+                return 0;
+            if(revnum<Integer.MIN_VALUE/10 || revnum<Integer.MIN_VALUE/10 && lastdig < -8)
+                return 0;
+
+            revnum = revnum*10 + lastdig;
+            x/=10;
+        }
+    return revnum;
+    }
+}
